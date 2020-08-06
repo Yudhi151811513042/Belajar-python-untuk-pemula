1fe269cb7a864035a94693ef65ed04b6aeb8a59d
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 14:52:26 +0700

    while loop

---
+++ while loop/while loop.py
@@ -0,0 +1,11 @@
+"""
+    LOOP WHILE
+"""
+count = 0
+
+while count < 5:
+    print("yeayyyy saya akan menikah")
+    count += 1
+
+else:
+    print("huft, akhirnya berhenti, countnya sekarang adalah", count)
