329805dbaf06c00f54261af163769c29e67d2c01
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 15:57:20 +0700

    kenalan dengan tuples

---
+++ kenalan dengan tuples/kenalan dengan tuples.py
@@ -0,0 +1,12 @@
+"""
+SEQUENCES
+ Lists - Tuples
+"""
+#immutable -> tidak bisa diedit
+
+orang = ("kresna","saputro","dali")
+orang2 = ["kresna","saputro","dali"]
+
+print(min(orang)) #mencari huruf awal terkecil
+print(list(orang)) # mengubah dari tuple menjadi list
+print(tuple(orang)) #mengubah dari list ke tuple
