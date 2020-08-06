c64e70da01ee30bb5c682d90f4f929dd9dd51647
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 13:21:26 +0700

    boolean true dan false

---
+++ boolean true dan false.py
@@ -0,0 +1,11 @@
+# boolean = true atau false
+
+angka1 = 10
+angka2 = 20
+
+print(angka1<angka2)
+print(angka1>angka2)
+
+text = "sekolahkoding"
+print(text.isdigit()) # mengecek apakah itu angka
+print(text.isalnum()) # mengecek apakah itu alphabet numeric
