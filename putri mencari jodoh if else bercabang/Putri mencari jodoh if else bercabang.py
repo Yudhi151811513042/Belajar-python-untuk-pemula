2a9a5e334132137bbfaac14c5a263ab2b813c5e5
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 14:41:58 +0700

    putri mencari jodoh if else bercabang

---
+++ putri mencari jodoh if else bercabang/Putri mencari jodoh if else bercabang.py
@@ -0,0 +1,15 @@
+"""
+Latihan : putri raja mencari jodoh
+sayaratnya : baik & RAJIN
+"""
+
+baik = True
+rajin = True
+tamu = "cewe"
+
+if baik & rajin & (tamu == "pria"):
+        print("kita nikah yuk")
+elif baik & rajin & (tamu == "cewe"):
+        print("kita jadi saudara yuk")
+else:
+    print("Hush! pergi sana")
