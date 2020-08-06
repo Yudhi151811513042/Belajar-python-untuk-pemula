62ca1daaa5778fe7002897fc33d2d4aa013e0703
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 11:46:42 +0700

    tipe data,string dan angka

---
+++ tipe data,string dan angka.py
@@ -0,0 +1,14 @@
+print('belajar python')
+# contoh salah -> print(belajar python)
+
+
+# type data angka
+print(10)
+print(10.5)
+x = (10)
+print (x)
+# cek type data
+type (x)
+type (10.5)
+x = '5' # type data string tidak bisa digunakan operasi matematika
+print (x)
