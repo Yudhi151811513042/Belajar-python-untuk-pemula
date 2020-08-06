1e08eaed12cad3f161b413e42c2a27ad5c77055e
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 12:24:06 +0700

    print angka dan string bersamaan

---
+++ print angka dan string bersamaan.py
@@ -0,0 +1,10 @@
+harga = 2500
+# error -> print ("total harganya" + harga)
+print("total harganya", harga)
+
+# convertion
+print("total harganya " + str(harga))
+
+x = "1"
+#error print(harga + x)
+print (harga + int(x))
