b7fc0a76af9764b2f32a730e0a216fc58d2d2ad1
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 15:28:32 +0700

    bermain dengan lists

---
+++ bermain dengan lists/bermain dengan lists.py
@@ -0,0 +1,13 @@
+orang = ["kresna", "saputro", "dali"]
+umur = [10,20,50,100,20]
+mixed = [1,"text", "editor", 2.5]
+
+    #index(angka perhitungan dimulai dari 0)
+
+orang.append('Thomson') # tambah list
+orang[2] = "Dalikerawan" #edit list
+del orang[2] # delete list
+
+print(orang)
+print(mixed)
+print(orang[1])
