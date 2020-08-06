d68e643e49c8fd7e51868a64cbb4512dd54bc153
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 12:08:47 +0700

    tipe data angka,metode dan math

---
+++ tipe data angka,metode dan math.py
@@ -0,0 +1,11 @@
+x = 10
+y = 20
+print(x+y)
+print (x * y)
+print(max(x,y)) # mencari angka tertinggi
+print(max(x,y,1,50,5))
+print(min(x,y,1,50,5)) # mencari angka terkecil
+print(round(7.89)) # membulatkan ke angka bulat terdekat
+print(round(7.19))
+import math # mengimpor fungsi math yang disediakan python
+print(math.pi)
