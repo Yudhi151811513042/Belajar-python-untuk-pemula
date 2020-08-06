762eb49eea5a601a318ecadef3840ce1ea8161b9
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-06 12:00:15 +0700

    tipe data string, metode, concatinate dan escaping

---
+++ tipe data string, metode, concatinate dan escaping.py
@@ -0,0 +1,21 @@
+# concatination
+x = "sekolah"
+print (x + " koding")
+y = "koding"
+print (x + y) # tanpa spasi
+print (x + " " + y)
+
+#Escaping
+
+print("halo halo\" bandung") # Gunakan tanda \ untuk menambah icon yang sma dengan penutup
+print("saya mau ada baris bar \n ini baris kedua") # \n tambah baris
+print("saya mau ada baris bar \t ini baris kedua") # \t tambah tab
+
+# menambah kapital di awal
+
+x = "sekolahkoding"
+print(x.capitalize())
+
+# Mengganti object
+print(x.replace("sekolah","rumah"))
+print(x.replace("k","c"))
