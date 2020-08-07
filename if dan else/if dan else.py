b987cc90341b934930621258bc7308fae4850d72
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-07 13:09:59 +0700

    if dan else

--- if dan else/if dan else.py
+++ if dan else/if dan else.py
@@ -4,7 +4,7 @@ angka2 = 20
 #proses dijalankan selama sesuai dlam intruksi if jika tidak sesuai maka akan masuk ke intruksi else
 
 if angka1 < angka2:
-    print("betul angka 1 lebih besar")
+    print("betul angka 1 lebih kecil")
     print("angka satu-nya adalah ", angka1)
 else:
     print("tetot!! salahh!!")
