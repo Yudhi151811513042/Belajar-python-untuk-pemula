0b78db6930e55028f9607d89722bcf0dce3779bc
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-07 10:25:19 +0700

    keyword argument

---
+++ keyword argument/keyword argument.py
@@ -0,0 +1,10 @@
+"""
+Keyword argument
+"""
+
+def printData(name, hobby):
+    print("Name: " + name + " - Hobby " + hobby)
+
+printData('hilman','nyanyi')
+
+# cara menampilkan jika tidak tahu urutan value printData(name = 'hilman', hobby = 'nyanyi')
